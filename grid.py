ee386d68683c3294fdd3192a3aec2cdbfeb7946d
Eowyn <eowyn.bridget@gmail.com>
2012-10-25 14:12:03 -0400

    refining the design

--- grid.py
+++ grid.py
@@ -1,7 +1,9 @@
 from kivy.uix.widget import Widget
 
 class Grid(Widget):
-    def __init__(self, template=None):
+    def __init__(self, **kwargs):
+        super(Grid, self).__init__(**kwargs)        
+        self.register_event_type('on_select')        
         self.cols = 0
         self.rows = 0
         self.selected = (0,0)
@@ -9,19 +11,22 @@ class Grid(Widget):
         self.bg_color = '#FFFFFF'
         self.text_color = '#000000'
     def make(self, rows=0, cols=0, template=None):
-        pass
+        for row in range(rows):
+            self.add_row
+            for col in range(cols):
+                #add cell (row, col)
     def reset(self):
-        """eliminate all rows, cols, contents, styles"""
-        pass
+        '''eliminate all rows, cols, contents, styles'''
+        
     def clear(self):
-        """remove all cell and label values"""
+        '''remove all cell and label values'''
         pass
     #grid setup/teardown
     def add_row(self, label=None, num=1, loc=self.rows):
-        """self.add_row(): insert one row at the bottom of the grid"""
+        '''self.add_row(): insert one row at the bottom of the grid'''
         self.rows += num
     def add_col(self, label=None, num=1, loc=self.cols):
-        """self.add_col(): insert one column at the right of the grid"""
+        '''self.add_col(): insert one column at the right of the grid'''
         self.cols += num
     def set_row_label_value(self, row, label):
         pass
@@ -45,13 +50,9 @@ class Grid(Widget):
         pass  
     
     #interaction
-    def sort_rows(self, col):
+    def sort(self, col):
         pass
-    def sort_cols(self, row):
-        pass
-    def filter_rows(self, col, value):
-        pass
-    def filter_cols(self, row, value):
+    def filter(self, col, value):
         pass
     def get_selection(self):
         pass
@@ -90,7 +91,7 @@ class Grid(Widget):
     
     #styles
     def set_bg(self, background):
-        """takes color (graphics too?) and makes it the grid background"""
+        '''takes color (graphics too?) and makes it the grid background'''
         pass
     def set_cell_bg(self, background):
         pass
@@ -100,7 +101,7 @@ class Grid(Widget):
                      border_width=1, border_color='#000000', border_type='solid'):
         pass
     def style_grid(self, cell_padding=3, line_width=1, line_type='solid', line_color='#000000'):
-        """all overall style settings should be possible to set through this shortcut method"""
+        '''all overall style settings should be possible to set through this shortcut method'''
         pass
     def style_border(self, width=1, type='solid', color='#000000'):
         pass
@@ -112,3 +113,12 @@ class Grid(Widget):
         pass
     def set_line_color(self, color):
         pass
+
+    #event-related
+    def select(self, data):
+        '''Call this method to trigger the `on_select` event, with the `data`
+        selection. The `data` can be anything you want.
+        '''
+        self.dispatch('on_select', data)
+        if self.dismiss_on_select:
+            self.dismiss()    
